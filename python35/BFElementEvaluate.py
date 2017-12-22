95b070a9f7c2082aaed3a555ad22f8f9fd024107
sanyueyuxincao <lm_hang@163.com>
2017-12-22 15:35:32 +0800

    change BFDemo

--- python35/BFElementEvaluate.py
+++ python35/BFElementEvaluate.py
@@ -1,13 +1,50 @@
 import re
+from BFStringDeal import BFStringDeal
+from lxml import etree
+import operator
+
 class BFElementEvaluate():
-    def __init__(self, htmlContent, section):
-        self.section = section
+    def __init__(self, htmlContent):
+        # 外界传入的htmlContent
         self.htmlContent = htmlContent
+        # 存放“候选人”基本信息的字典， key值为在root中的div元素出现的序列号 == value值代表改div元素的string表达
+        self.candidataBaseInfoDict = dict()
+        # 存放“候选人”打分结果的字典，key值为在root中div元素出现的序列号 == value值代表该div元素的打分结果
+        self.candidateEvaluateDict = dict()
+        # 获取文档最底层的xpath node
+        self.rootNode = etree.HTML(htmlContent)
+    # 获取文章主体部分内容
+    def getMainContent(self):
+        # 初始化。实例变量的值
+        self.candidataBaseInfoDict = {}
+        self.candidateEvaluateDict = {}
 
-    def getXpathLocation(self):
-        if (self.section == "article"):
-            # 如果我们需要获取文章中，最主体核心的部分
+        # 存放所有divNODE的列表 -- 基本上所有mainContent一定是div结构
+        divHandleNode = self.rootNode.xpath("//div")
+
+        # 处理候选人基本信息
+        for i in range(0,len(divHandleNode)):
+            divHandleText = etree.tostring(divHandleNode[i]).decode("utf-8")
+            # print(divHandleText)
+            # 很多时候，由于div元素过多，所以，我们在这里需要进行一个，最基本判断，如果没有</p>直接没有竞选资格
+            if "</p>" in divHandleText:
+                # print(divHandleText)
+                self.candidataBaseInfoDict.update({i:divHandleText})
 
 
-        else:
-            print("present only support article part")
+        # 处理候选人打分结果字典
+        for key in self.candidataBaseInfoDict:
+            textLengthValue = len(self.candidataBaseInfoDict[key]) * 2
+            self.candidateEvaluateDict.update({key:textLengthValue})
+
+
+        # 查看下候选人基本信息
+        # print(self.candidataBaseInfoDict)
+
+        # 查看下候选人的打分情况
+        # print(self.candidateEvaluateDict)
+
+        # 评分系统, 得到最终候选人的node节点位置，基于root
+        contentNodeNumber = max(self.candidateEvaluateDict.items(), key=operator.itemgetter(1))[0]
+
+        print(etree.tostring(divHandleNode[contentNodeNumber]).decode("utf-8"))
--- python35/justForTest.py
+++ python35/justForTest.py
@@ -34,49 +34,22 @@ originalString = "<div class=\"outsiteone\"><p>p logo</p>outSideOne<div class=\"
 
 # rootNode = etree.HTML(originalString).xpath("//div[contains(@class, \"insidetwo\")]")[0].tag
 
-# 存放“候选人”基本信息的字典， key值为在root中的div元素出现的序列号 == value值代表改div元素的string表达
-candidataBaseInfoDict = dict()
 
-# 存放“候选人”打分结果的字典，key值为在root中div元素出现的序列号 == value值代表该div元素的打分结果
-candidateEvaluateDict = dict()
 
-# 获取文档最底层的xpath node
-rootNode = etree.HTML(originalString)
 
-# 存放所有divNODE的列表
-divHandleNode = rootNode.xpath("//div")
 
-#####
-xxxx = "<div>xxxxxxxxxxx<div>yyyyyyy</div>xxxx</div>"
-yyy = BFStringDeal.deleteAllTag(xxxx)
 
-print(yyy)
+#
+# #####
+# xxxx = "<div>xxxxxxxxxxx<div>yyyyyyy</div>xxxx</div>"
+# yyy = BFStringDeal.deleteAllTag(xxxx)
+#
+# print(yyy)
+#
+# ####
 
-####
 
-# 处理候选人基本信息
-for i in range(0,len(divHandleNode)):
-    divHandleText = etree.tostring(divHandleNode[i]).decode("utf-8")
-    print(divHandleText)
-    # 很多时候，由于div元素过多，所以，我们在这里需要进行一个，最基本判断，如果没有</p>直接没有竞选资格
-    if "</p>" in divHandleText:
-        print(divHandleText)
-        candidataBaseInfoDict.update({i:divHandleText})
 
-# 处理候选人打分结果字典
-for key in candidataBaseInfoDict:
-    textLengthValue = len(candidataBaseInfoDict[key]) * 2
-    candidateEvaluateDict.update({key:textLengthValue})
 
 
 # 最后我们取出最高分数
-
-
-# 查看下候选人基本信息
-print(candidataBaseInfoDict)
-
-# 查看下候选人的打分情况
-print(candidateEvaluateDict)
-
-# 评分系统, 得到最终候选人的node节点位置，基于root
-print(max(candidateEvaluateDict.items(), key=operator.itemgetter(1))[0])
