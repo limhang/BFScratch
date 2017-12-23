b4cd42e18c7348c965e551bffa3f4b6c36f9c98e
limhang <lm_hang@163.com>
2017-12-23 22:00:54 +0800

    normative comment

--- src/BFFileSystem.py
+++ src/BFFileSystem.py
@@ -3,7 +3,7 @@ class BFFileSystem():
         self.arg = arg
 
     @classmethod
-    # 写入数据到指定文件
+    # 写入数据到指定文件，其中'a'为append的意思就是往后加
     def writeDataToFile(cls, content, fileName):
         with open(fileName,'a') as f:
             f.write(content + '\n')
--- src/BFLocateElement.py
+++ src/BFLocateElement.py
@@ -1,26 +1,39 @@
 from lxml import etree
 
-
-# 元素定位获取模块
-# 这个模块将要细分为2个组成部分
-# section one -- 定位解析字符串 【dom】
-# section two -- 定位解析json  【json】
 class BFLocateElement():
-    def __init__(self, contentType, content, originalSource):
-        # 支持2种类型，一是json解析；二是纯text的html文本(requests.text获取的)
-        self.type = contentType
-        # 用户在浏览器中，通过xpath helper获取到的定位元素的表达
-        self.content = content
-        # BFRequest模块中传入的text解析文本
+    """BFLocateElement overview
+
+    enter the precise positioning information of the element in the document
+    obtained through xpath and output the positioning element content.
+
+    Attributes:
+        contentType: dom or json indicate the type of data to be parsed
+        analyzingConditions: based on this positioning element
+        originalSource: input data source
+    """
+
+    def __init__(self, contentType, analyzingConditions, originalSource):
+        """init method
+        the most basic initialization function. all the args has been explained
+        in the overview.
+        """
+        self.contentType = contentType
+        self.analyzingConditions = analyzingConditions
         self.originalSource = originalSource
 
-    # 返回给外界解析后的数据
     def locate(self):
-        # 传入的是dom文本的情况
-        if (self.type == 'dom'):
-            node = etree.HTML(self.originalSource).xpath(self.content)[0]
+        """the core part of positioning operations
+
+        Args:
+            null
+
+        Returns:
+            the contents when positioning operation is completed
+        """
+        if (self.contentType == 'dom'):
+            node = etree.HTML(self.originalSource).xpath(self.analyzingConditions)[0]
             return etree.tostring(node).decode("utf-8")
-        elif (self.type == 'json'):
+        elif (self.contentType == 'json'):
             print('use json')
         else:
             print('present only support json and dom analysis')
